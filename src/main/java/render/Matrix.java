09bc173300b37e1500a0f58f5a4e655914618be8
Andrey Lavrentiev <fatalspirit335@gmail.com>
2024-01-28 21:32:02 +0300

    get() & set()

--- src/main/java/render/Matrix.java
+++ src/main/java/render/Matrix.java
@@ -4,11 +4,22 @@ import java.awt.*;
 
 class Matrix {
 
- private Images[][] matrix;
+    private Images[][] matrix;
 
     Matrix(Images defaultImage) {
         matrix = new Images[MapRanges.getSize().x][MapRanges.getSize().y];
         for (Coordinate coordinate : MapRanges.getAllCoordinates())
             matrix[coordinate.x][coordinate.y] = defaultImage;
+    }
+
+    Images get(Coordinate coordinate) {
+        if (MapRanges.inRange(coordinate))
+            return matrix[coordinate.x][coordinate.y];
+        return null;
+    }
+
+    void set(Coordinate coordinate, Images images) {
+        if (MapRanges.inRange(coordinate))
+            matrix[coordinate.x][coordinate.y] = images;
     }
 }
