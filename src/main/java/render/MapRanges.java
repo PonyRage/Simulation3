007c03675480ca558419475248076f1e35234449
Andrey Lavrentiev <fatalspirit335@gmail.com>
2024-01-28 20:33:46 +0300

    add ArrayList<Cordinate> in class and add in constructor

--- src/main/java/render/MapRanges.java
+++ src/main/java/render/MapRanges.java
@@ -1,9 +1,11 @@
 package render;
 
 
+import java.util.ArrayList;
 
 public class MapRanges {
     private static Coordinate size;
+    private static ArrayList<Coordinate> allCoordinates;
 
     public static Coordinate getSize() {
         return size;
@@ -11,5 +13,11 @@ public class MapRanges {
 
     public static void setSize(Coordinate _size) {
         size = _size;
+        allCoordinates = new ArrayList<Coordinate>();
+        for (int y = 0; y < size.y; y++) {
+            for (int x = 0; x < size.x; x++) {
+                allCoordinates.add(new Coordinate(x, y));
+            }
+        }
     }
 }
